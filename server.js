93f6577f13dc2d478b89705d4b1e62fd2d3a9133
ismaely <gunza.ismael@gmail.com>
2020-11-27 21:34:27 +0100

    servidor inserido

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.use(express.static(__dirname + '/dist/site-h5'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/site-h5/index.html');
+})
+
+
+app.listen(PORT, () => console.log(`Servidor up`));
