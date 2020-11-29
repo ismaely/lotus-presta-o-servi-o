a65673dc9dc2b26145b1e9a6336f20b989559882
ismaely <gunza.ismael@gmail.com>
2020-11-29 15:04:00 +0100

    ajusto do tema e menu

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express')
 const app = express()
-
 const PORT = process.env.PORT || 3000;
 
-//app.get('/', (req, res) => res.send('Hello World!'))
-
-
 app.use(express.static(__dirname + '/dist/site-h5'));
-
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/site-h5/index.html');
 })
-
-
 app.listen(PORT, () => console.log(`Servidor up`));
