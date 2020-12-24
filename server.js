5ec269a7f27a75ae725a108f96a0fca8857922d3
ismaely <gunza.ismael@gmail.com>
2020-12-24 14:54:56 +0100

    finalizado

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist/site-h5'));
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/site-h5/index.html');
-})
+});
 app.listen(PORT, () => console.log(`Servidor up`));
